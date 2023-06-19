2d85952d7b16a46a5ca6159fb412b58c25cec856
unknown <gm.c.burzo@gmail.com>
2023-06-19 10:16:49 -0400

    i

---
+++ javaHTTPServer/src/main/java/com/chrisServer/httpServer/util/Json.java
@@ -0,0 +1,7 @@
+package com.chrisServer.httpServer.util;
+import com.fasterxml.jackson.databind.ObjectMapper;
+public class Json {
+    private static ObjectMapper myObjectMapper;
+
+    
+}
--- javaHTTPServer/src/main/java/com/chrisServer/httpServer/util/json.java
+++
@@ -1,5 +0,0 @@
-package com.chrisServer.httpServer.util;
-
-public class json {
-
-}
