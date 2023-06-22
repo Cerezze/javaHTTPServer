9346288b8e385786c48daf2778b8aacc44ad850a
unknown <gm.c.burzo@gmail.com>
2023-06-22 17:48:57 -0400

    i

--- javaHTTPServer/src/main/java/com/chrisServer/httpServer/HttpServer.java
+++ javaHTTPServer/src/main/java/com/chrisServer/httpServer/HttpServer.java
@@ -1,7 +1,51 @@
 package com.chrisServer.httpServer;
 
+import com.chrisServer.httpServer.config.Configuration;
+import com.chrisServer.httpServer.config.ConfigurationManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
     public static void main(String[] args){
         System.out.println("Server Starting");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Using Port: " + conf.getPort());
+        System.out.println("Using WebRoot: " + conf.getWebroot());
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was sent using my simple http java server</h1></body></html>";
+
+            final String CRLF = "\n\r"; //13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +  //Status Line: HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + //HEADER
+                            CRLF +
+                            html +
+                            CRLF +
+                            CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- javaHTTPServer/src/main/java/com/chrisServer/httpServer/config/ConfigurationManager.java
+++ javaHTTPServer/src/main/java/com/chrisServer/httpServer/config/ConfigurationManager.java
@@ -1,5 +1,12 @@
 package com.chrisServer.httpServer.config;
 
+import com.chrisServer.httpServer.util.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
     private static ConfigurationManager myConfigurationManager;
     private static Configuration myCurrentConfiguration;
@@ -18,15 +25,45 @@ public class ConfigurationManager {
     /*
     * Used to load a configuration file by the path provided
     * */
-    public void loadConfigurationFile(String filePath){
+    public void loadConfigurationFile(String filePath) {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch(FileNotFoundException e) {
+            throw new HttpConfigurationException(e);
+        }
+        StringBuffer sb = new StringBuffer();
+        int i = 0;
+        try{
+            while((i = fileReader.read()) != -1){
+                sb.append((char)i);
+            }
+        }catch (IOException e){
+            throw new HttpConfigurationException(e);
+        }
 
+        try{
+            JsonNode conf = Json.parse(sb.toString());
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        }catch(IOException e){
+            throw new HttpConfigurationException("Error Parsing the Configuration File");
+        }
+        try{
+            JsonNode conf = Json.parse(sb.toString());
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        }catch(IOException e){
+            throw new HttpConfigurationException("Error Parsing the Configuration File, internal", e);
+        }
     }
 
     /*
     *  Returns the Current loaded Configuration
     * */
 
-    public void getCurrentConfiguration(){
-
+    public Configuration getCurrentConfiguration(){
+        if(myCurrentConfiguration == null){
+            throw new HttpConfigurationException("No Current Configuration Set.");
+        }
+        return myCurrentConfiguration;
     }
 }
