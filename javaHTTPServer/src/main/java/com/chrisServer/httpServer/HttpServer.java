7a816b315f83417fd0b38a589cd3c52223bf34db
unknown <gm.c.burzo@gmail.com>
2023-06-23 19:08:03 -0400

    i

--- javaHTTPServer/src/main/java/com/chrisServer/httpServer/HttpServer.java
+++ javaHTTPServer/src/main/java/com/chrisServer/httpServer/HttpServer.java
@@ -2,15 +2,12 @@ package com.chrisServer.httpServer;
 
 import com.chrisServer.httpServer.config.Configuration;
 import com.chrisServer.httpServer.config.ConfigurationManager;
+import com.chrisServer.httpServer.core.ServerListenerThread;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class HttpServer {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Server Starting");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
@@ -20,31 +17,9 @@ public class HttpServer {
         System.out.println("Using WebRoot: " + conf.getWebroot());
 
         try{
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was sent using my simple http java server</h1></body></html>";
-
-            final String CRLF = "\n\r"; //13, 10
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF +  //Status Line: HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-Length: " + html.getBytes().length + CRLF + //HEADER
-                            CRLF +
-                            html +
-                            CRLF +
-                            CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-        }catch(IOException e) {
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
